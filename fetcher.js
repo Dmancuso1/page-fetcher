469b926ee1a55ce3cea31350f760f70671021631
Dane Mancuso <danegmancuso@gmail.com>
2020-07-02 01:26:32 +0000

    removed indexindexindex.html

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,7 @@
+
+// Implement a small command line node app called fetcher.js which should take a URL as a command-line argument as well as a local file path and download the resource to the specified path.
+
+
 const request = require('request');
 const fs = require('fs');
 const arg = process.argv.slice(2);
